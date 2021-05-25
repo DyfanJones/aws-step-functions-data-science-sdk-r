a7595de1f7b4ceb58c16c5137e28106ac3ff80fb
DyfanJones <dyfan.jones@shopdirect.com>
2021-05-25 13:16:00 +0100

    feature: add code build stepfunction task (#5)

--- R/steps_service.R
+++ R/steps_service.R
@@ -12,6 +12,7 @@ DYNAMODB_SERVICE_NAME = "dynamodb"
 SNS_SERVICE_NAME = "sns"
 SQS_SERVICE_NAME = "sqs"
 ELASTICMAPREDUCE_SERVICE_NAME = "elasticmapreduce"
+CODEBUILD_SERVICE_NAME = "codebuild"
 
 DynamoDBApi = Enum(
   GetItem = "getItem",
@@ -36,6 +37,13 @@ ElasticMapReduceApi = Enum(
   SetClusterTerminationProtection = "setClusterTerminationProtection",
   ModifyInstanceFleetByName = "modifyInstanceFleetByName",
   ModifyInstanceGroupByName = "modifyInstanceGroupByName"
+)
+
+CodeBuildApi = Enum(
+  StartBuild = "startBuild",
+  StopBuild = "stopBuild",
+  BatchDeleteBuilds = "batchDeleteBuilds",
+  BatchGetReports = "batchGetReports"
 )
 
 #' @title DynamoDBGetItemStep class
@@ -936,3 +944,310 @@ EmrModifyInstanceFleetByNameStep = R6Class("EmrModifyInstanceFleetByNameStep",
   ),
   lock_objects=F
 )
+
+#' @title CodeBuildStartBuildStep class
+#' @description Creates a Task state to start code build jobs. See Call Amazon Code Build
+#'              with Step Functions \url{https://docs.aws.amazon.com/step-functions/latest/dg/connect-codebuild.html}
+#'              for more details.
+#' @export
+CodeBuildStartBuildStep = R6Class("CodeBuildStartBuildStep",
+  inherit = Task,
+  public = list(
+
+    #' @description Initialize CodeBuildStartBuildStep task class
+    #' @param state_id (str): State name whose length **must be** less than or equal
+    #'               to 128 unicode characters. State names **must be** unique within
+    #'               the scope of the whole state machine.
+    #' @param wait_for_completion (bool, optional): Boolean value set to `True` if
+    #'              the Task state should wait for the glue job to complete before proceeding
+    #'              to the next step in the workflow. Set to `False` if the Task state should
+    #'              submit the glue job and proceed to the next step. (default: True)
+    #' @param comment (str, optional): Human-readable comment or description. (default: None)
+    #' @param timeout_seconds (int, optional): Positive integer specifying timeout
+    #'              for the state in seconds. If the state runs longer than the specified
+    #'              timeout, then the interpreter fails the state with a `States.Timeout`
+    #'              Error Name. (default: 60)
+    #' @param timeout_seconds_path (str, optional): Path specifying the state's timeout
+    #'              value in seconds from the state input. When resolved, the path must
+    #'              select a field whose value is a positive integer.
+    #' @param heartbeat_seconds (int, optional): Positive integer specifying heartbeat
+    #'              timeout for the state in seconds. This value should be lower than
+    #'              the one specified for `timeout_seconds`. If more time than the specified
+    #'              heartbeat elapses between heartbeats from the task, then the interpreter
+    #'              fails the state with a `States.Timeout` Error Name.
+    #' @param heartbeat_seconds_path (str, optional): Path specifying the state's heartbeat
+    #'              value in seconds from the state input. When resolved, the path must select
+    #'              a field whose value is a positive integer.
+    #' @param input_path (str, optional): Path applied to the state’s raw input to
+    #'              select some or all of it; that selection is used by the state. (default: '$')
+    #' @param parameters (list, optional): The value of this field becomes the effective
+    #'              input for the state.
+    #' @param result_path (str, optional): Path specifying the raw input’s combination
+    #'              with or replacement by the state’s result. (default: '$')
+    #' @param output_path (str, optional): Path applied to the state’s output after
+    #'              the application of `result_path`, producing the effective output
+    #'              which serves as the raw input for the next state. (default: '$')
+    #' @param ... : Extra Fields passed to Task class
+    initialize = function(state_id,
+                          comment=NULL,
+                          wait_for_completion=TRUE,
+                          timeout_seconds=NULL,
+                          timeout_seconds_path=NULL,
+                          heartbeat_seconds=NULL,
+                          heartbeat_seconds_path=NULL,
+                          input_path=NULL,
+                          parameters=NULL,
+                          result_path=NULL,
+                          output_path=NULL,
+                          ...){
+      kwargs = list(
+      state_id=state_id,
+      timeout_seconds=timeout_seconds,
+      timeout_seconds_path=timeout_seconds_path,
+      heartbeat_seconds=heartbeat_seconds,
+      heartbeat_seconds_path=heartbeat_seconds_path,
+      comment=comment,
+      input_path=input_path,
+      parameters=parameters,
+      result_path=result_path,
+      output_path=output_path,
+      ...)
+
+      if (wait_for_completion)
+        kwargs[[Field$Resource]] = get_service_integration_arn(
+          CODEBUILD_SERVICE_NAME,
+          CodeBuildApi$StartBuild,
+          IntegrationPattern$WaitForCompletion)
+      else
+        kwargs[[Field$Resource]] = get_service_integration_arn(
+          CODEBUILD_SERVICE_NAME,
+          CodeBuildApi$StartBuild)
+
+      do.call(super$initialize, kwargs)
+    }
+  ),
+  lock_objects=F
+)
+
+#' @title CodeBuildStopBuildStep class
+#' @description Creates a Task state to stop code build jobs. See Call Amazon Code Build
+#'              with Step Functions \url{https://docs.aws.amazon.com/step-functions/latest/dg/connect-codebuild.html}
+#'              for more details.
+#' @export
+CodeBuildStopBuildStep = R6Class("CodeBuildStopBuildStep",
+  inherit = Task,
+  public = list(
+
+    #' @description Initialize CodeBuildStopBuildStep task class
+    #' @param state_id (str): State name whose length **must be** less than or equal
+    #'               to 128 unicode characters. State names **must be** unique within
+    #'               the scope of the whole state machine.
+    #' @param comment (str, optional): Human-readable comment or description. (default: None)
+    #' @param timeout_seconds (int, optional): Positive integer specifying timeout
+    #'              for the state in seconds. If the state runs longer than the specified
+    #'              timeout, then the interpreter fails the state with a `States.Timeout`
+    #'              Error Name. (default: 60)
+    #' @param timeout_seconds_path (str, optional): Path specifying the state's timeout
+    #'              value in seconds from the state input. When resolved, the path must
+    #'              select a field whose value is a positive integer.
+    #' @param heartbeat_seconds (int, optional): Positive integer specifying heartbeat
+    #'              timeout for the state in seconds. This value should be lower than
+    #'              the one specified for `timeout_seconds`. If more time than the specified
+    #'              heartbeat elapses between heartbeats from the task, then the interpreter
+    #'              fails the state with a `States.Timeout` Error Name.
+    #' @param heartbeat_seconds_path (str, optional): Path specifying the state's heartbeat
+    #'              value in seconds from the state input. When resolved, the path must select
+    #'              a field whose value is a positive integer.
+    #' @param input_path (str, optional): Path applied to the state’s raw input to
+    #'              select some or all of it; that selection is used by the state. (default: '$')
+    #' @param parameters (list, optional): The value of this field becomes the effective
+    #'              input for the state.
+    #' @param result_path (str, optional): Path specifying the raw input’s combination
+    #'              with or replacement by the state’s result. (default: '$')
+    #' @param output_path (str, optional): Path applied to the state’s output after
+    #'              the application of `result_path`, producing the effective output
+    #'              which serves as the raw input for the next state. (default: '$')
+    #' @param ... : Extra Fields passed to Task class
+    initialize = function(state_id,
+                          comment=NULL,
+                          timeout_seconds=NULL,
+                          timeout_seconds_path=NULL,
+                          heartbeat_seconds=NULL,
+                          heartbeat_seconds_path=NULL,
+                          input_path=NULL,
+                          parameters=NULL,
+                          result_path=NULL,
+                          output_path=NULL,
+                          ...){
+      kwargs = list(
+        state_id=state_id,
+        timeout_seconds=timeout_seconds,
+        timeout_seconds_path=timeout_seconds_path,
+        heartbeat_seconds=heartbeat_seconds,
+        heartbeat_seconds_path=heartbeat_seconds_path,
+        comment=comment,
+        input_path=input_path,
+        parameters=parameters,
+        result_path=result_path,
+        output_path=output_path,
+        ...)
+
+      kwargs[[Field$Resource]] = get_service_integration_arn(
+        CODEBUILD_SERVICE_NAME,
+        CodeBuildApi$stopBuild)
+
+      do.call(super$initialize, kwargs)
+    }
+  ),
+  lock_objects=F
+)
+
+#' @title CodeBuildBatchDeleteBuildsStep class
+#' @description Creates a Task state to stop code build jobs. See Call Amazon Code Build
+#'              with Step Functions \url{https://docs.aws.amazon.com/step-functions/latest/dg/connect-codebuild.html}
+#'              for more details.
+#' @export
+CodeBuildBatchDeleteBuildsStep = R6Class("CodeBuildBatchDeleteBuildsStep",
+  inherit = Task,
+  public = list(
+
+    #' @description Initialize CodeBuildBatchDeleteBuildsStep task class
+    #' @param state_id (str): State name whose length **must be** less than or equal
+    #'               to 128 unicode characters. State names **must be** unique within
+    #'               the scope of the whole state machine.
+    #' @param comment (str, optional): Human-readable comment or description. (default: None)
+    #' @param timeout_seconds (int, optional): Positive integer specifying timeout
+    #'              for the state in seconds. If the state runs longer than the specified
+    #'              timeout, then the interpreter fails the state with a `States.Timeout`
+    #'              Error Name. (default: 60)
+    #' @param timeout_seconds_path (str, optional): Path specifying the state's timeout
+    #'              value in seconds from the state input. When resolved, the path must
+    #'              select a field whose value is a positive integer.
+    #' @param heartbeat_seconds (int, optional): Positive integer specifying heartbeat
+    #'              timeout for the state in seconds. This value should be lower than
+    #'              the one specified for `timeout_seconds`. If more time than the specified
+    #'              heartbeat elapses between heartbeats from the task, then the interpreter
+    #'              fails the state with a `States.Timeout` Error Name.
+    #' @param heartbeat_seconds_path (str, optional): Path specifying the state's heartbeat
+    #'              value in seconds from the state input. When resolved, the path must select
+    #'              a field whose value is a positive integer.
+    #' @param input_path (str, optional): Path applied to the state’s raw input to
+    #'              select some or all of it; that selection is used by the state. (default: '$')
+    #' @param parameters (list, optional): The value of this field becomes the effective
+    #'              input for the state.
+    #' @param result_path (str, optional): Path specifying the raw input’s combination
+    #'              with or replacement by the state’s result. (default: '$')
+    #' @param output_path (str, optional): Path applied to the state’s output after
+    #'              the application of `result_path`, producing the effective output
+    #'              which serves as the raw input for the next state. (default: '$')
+    #' @param ... : Extra Fields passed to Task class
+    initialize = function(state_id,
+                          comment=NULL,
+                          timeout_seconds=NULL,
+                          timeout_seconds_path=NULL,
+                          heartbeat_seconds=NULL,
+                          heartbeat_seconds_path=NULL,
+                          input_path=NULL,
+                          parameters=NULL,
+                          result_path=NULL,
+                          output_path=NULL,
+                          ...){
+      kwargs = list(
+        state_id=state_id,
+        timeout_seconds=timeout_seconds,
+        timeout_seconds_path=timeout_seconds_path,
+        heartbeat_seconds=heartbeat_seconds,
+        heartbeat_seconds_path=heartbeat_seconds_path,
+        comment=comment,
+        input_path=input_path,
+        parameters=parameters,
+        result_path=result_path,
+        output_path=output_path,
+        ...)
+
+      kwargs[[Field$Resource]] = get_service_integration_arn(
+        CODEBUILD_SERVICE_NAME,
+        CodeBuildApi$BatchDeleteBuilds)
+
+      do.call(super$initialize, kwargs)
+    }
+  ),
+  lock_objects=F
+)
+
+#' @title CodeBuildBatchGetReportsStep class
+#' @description Creates a Task state to stop code build jobs. See Call Amazon Code Build
+#'              with Step Functions \url{https://docs.aws.amazon.com/step-functions/latest/dg/connect-codebuild.html}
+#'              for more details.
+#' @export
+CodeBuildBatchGetReportsStep = R6Class("CodeBuildBatchGetReportsStep",
+  inherit = Task,
+  public = list(
+
+    #' @description Initialize CodeBuildBatchGetReportsStep task class
+    #' @param state_id (str): State name whose length **must be** less than or equal
+    #'               to 128 unicode characters. State names **must be** unique within
+    #'               the scope of the whole state machine.
+    #' @param comment (str, optional): Human-readable comment or description. (default: None)
+    #' @param timeout_seconds (int, optional): Positive integer specifying timeout
+    #'              for the state in seconds. If the state runs longer than the specified
+    #'              timeout, then the interpreter fails the state with a `States.Timeout`
+    #'              Error Name. (default: 60)
+    #' @param timeout_seconds_path (str, optional): Path specifying the state's timeout
+    #'              value in seconds from the state input. When resolved, the path must
+    #'              select a field whose value is a positive integer.
+    #' @param heartbeat_seconds (int, optional): Positive integer specifying heartbeat
+    #'              timeout for the state in seconds. This value should be lower than
+    #'              the one specified for `timeout_seconds`. If more time than the specified
+    #'              heartbeat elapses between heartbeats from the task, then the interpreter
+    #'              fails the state with a `States.Timeout` Error Name.
+    #' @param heartbeat_seconds_path (str, optional): Path specifying the state's heartbeat
+    #'              value in seconds from the state input. When resolved, the path must select
+    #'              a field whose value is a positive integer.
+    #' @param input_path (str, optional): Path applied to the state’s raw input to
+    #'              select some or all of it; that selection is used by the state. (default: '$')
+    #' @param parameters (list, optional): The value of this field becomes the effective
+    #'              input for the state.
+    #' @param result_path (str, optional): Path specifying the raw input’s combination
+    #'              with or replacement by the state’s result. (default: '$')
+    #' @param output_path (str, optional): Path applied to the state’s output after
+    #'              the application of `result_path`, producing the effective output
+    #'              which serves as the raw input for the next state. (default: '$')
+    #' @param ... : Extra Fields passed to Task class
+    initialize = function(state_id,
+                          comment=NULL,
+                          timeout_seconds=NULL,
+                          timeout_seconds_path=NULL,
+                          heartbeat_seconds=NULL,
+                          heartbeat_seconds_path=NULL,
+                          input_path=NULL,
+                          parameters=NULL,
+                          result_path=NULL,
+                          output_path=NULL,
+                          ...){
+      kwargs = list(
+        state_id=state_id,
+        timeout_seconds=timeout_seconds,
+        timeout_seconds_path=timeout_seconds_path,
+        heartbeat_seconds=heartbeat_seconds,
+        heartbeat_seconds_path=heartbeat_seconds_path,
+        comment=comment,
+        input_path=input_path,
+        parameters=parameters,
+        result_path=result_path,
+        output_path=output_path,
+        ...)
+
+      kwargs[[Field$Resource]] = get_service_integration_arn(
+        CODEBUILD_SERVICE_NAME,
+        CodeBuildApi$BatchGetReports)
+
+      do.call(super$initialize, kwargs)
+    }
+  ),
+  lock_objects=F
+)
+
+
+
+
